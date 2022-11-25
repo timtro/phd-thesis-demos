6b6c3371e3338c58e94bbcea3f8a407b26c3bfbb
timtro <timtro@gmail.com>
2022-11-25 01:10:46 -0500

    Renamed to use variables from thesis.

--- src/mealy-fp.cpp
+++ src/mealy-fp.cpp
@@ -11,99 +11,99 @@
 #include <vector>
 
 using C = int;
-using U = int;
-using X = int;
+using O = int;
+using I = int;
 
-C f(C c, X x) { return c + x * x; }
+C f(C c, I x) { return c + 1 * x; }
 
-U r(C c) { return 2 * c; }
+O r(C c) { return 1 * c; }
 
 class MFP {
   C c;
   MFP(C c) : c{c} {}
 
  public:
-  static std::pair<U, MFP> make_MFP(C c0) { return {r(c0), MFP{c0}}; }
+  static std::pair<O, MFP> make_MFP(C c0) { return {r(c0), MFP{c0}}; }
 
-  std::pair<U, MFP> operator()(X x) const { return make_MFP(f(c, x)); }
+  std::pair<O, MFP> operator()(I x) const { return make_MFP(f(c, x)); }
 };
 
-void by_hand(std::vector<X> xs) {
+void by_hand(std::vector<I> is) {
   const auto [u0, l0] = MFP::make_MFP(0);
-  const auto [u1, l1] = l0(xs[0]);
-  const auto [u2, l2] = l1(xs[1]);
-  const auto [u3, l3] = l2(xs[2]);
-  const auto [u4, l4] = l3(xs[3]);
-  const auto [u5, l5] = l4(xs[4]);
-  const auto [u6, l6] = l5(xs[5]);
-  const auto [u7, l7] = l6(xs[6]);
-  const auto [u8, l8] = l7(xs[7]);
-  const auto [u9, l9] = l8(xs[8]);
-  const auto [u10, l10] = l9(xs[9]);
+  const auto [u1, l1] = l0(is[0]);
+  const auto [u2, l2] = l1(is[1]);
+  const auto [u3, l3] = l2(is[2]);
+  const auto [u4, l4] = l3(is[3]);
+  const auto [u5, l5] = l4(is[4]);
+  const auto [u6, l6] = l5(is[5]);
+  const auto [u7, l7] = l6(is[6]);
+  const auto [u8, l8] = l7(is[7]);
+  const auto [u9, l9] = l8(is[8]);
+  const auto [u10, l10] = l9(is[9]);
 
   std::cout << u0 << ' ' << u1 << ' ' << u2 << ' ' << u3 << ' ' << u4 << ' '
             << u5 << ' ' << u6 << ' ' << u7 << ' ' << u8 << ' ' << u9 << ' '
             << u10 << '\n';
 }
 
-void by_hand_as_list(std::vector<X> xs) {
-  std::vector<U> us = {
+void by_hand_as_list(std::vector<I> is) {
+  std::vector<O> os = {
       r(0),
-      r(f(0, xs[0])),
-      r(f(f(0, xs[0]), xs[1])),
-      r(f(f(f(0, xs[0]), xs[1]), xs[2])),
-      r(f(f(f(f(0, xs[0]), xs[1]), xs[2]), xs[3])),
-      r(f(f(f(f(f(0, xs[0]), xs[1]), xs[2]), xs[3]), xs[4])),
-      r(f(f(f(f(f(f(0, xs[0]), xs[1]), xs[2]), xs[3]), xs[4]), xs[5])),
-      r(f(f(f(f(f(f(f(0, xs[0]), xs[1]), xs[2]), xs[3]), xs[4]), xs[5]),
-          xs[6])),
-      r(f(f(f(f(f(f(f(f(0, xs[0]), xs[1]), xs[2]), xs[3]), xs[4]), xs[5]),
-            xs[6]),
-          xs[7])),
-      r(f(f(f(f(f(f(f(f(f(0, xs[0]), xs[1]), xs[2]), xs[3]), xs[4]), xs[5]),
-              xs[6]),
-            xs[7]),
-          xs[8])),
-      r(f(f(f(f(f(f(f(f(f(f(0, xs[0]), xs[1]), xs[2]), xs[3]), xs[4]), xs[5]),
-                xs[6]),
-              xs[7]),
-            xs[8]),
-          xs[9]))};
-  for (auto each : us) std::cout << each << ' ';
+      r(f(0, is[0])),
+      r(f(f(0, is[0]), is[1])),
+      r(f(f(f(0, is[0]), is[1]), is[2])),
+      r(f(f(f(f(0, is[0]), is[1]), is[2]), is[3])),
+      r(f(f(f(f(f(0, is[0]), is[1]), is[2]), is[3]), is[4])),
+      r(f(f(f(f(f(f(0, is[0]), is[1]), is[2]), is[3]), is[4]), is[5])),
+      r(f(f(f(f(f(f(f(0, is[0]), is[1]), is[2]), is[3]), is[4]), is[5]),
+          is[6])),
+      r(f(f(f(f(f(f(f(f(0, is[0]), is[1]), is[2]), is[3]), is[4]), is[5]),
+            is[6]),
+          is[7])),
+      r(f(f(f(f(f(f(f(f(f(0, is[0]), is[1]), is[2]), is[3]), is[4]), is[5]),
+              is[6]),
+            is[7]),
+          is[8])),
+      r(f(f(f(f(f(f(f(f(f(f(0, is[0]), is[1]), is[2]), is[3]), is[4]), is[5]),
+                is[6]),
+              is[7]),
+            is[8]),
+          is[9]))};
+  for (auto each : os) std::cout << each << ' ';
   std::cout << '\n';
 }
 
-void lib_cpp(std::vector<X> xs) {
-  std::vector<int> output(xs.size());
-  std::exclusive_scan(cbegin(xs), cend(xs), begin(output), 0, f);
+void lib_cpp(std::vector<I> is) {
+  std::vector<int> output(is.size());
+  std::exclusive_scan(cbegin(is), cend(is), begin(output), 0, f);
   std::transform(cbegin(output), cend(output), begin(output), r);
   for (auto each : output) std::cout << each << ' ';
   std::cout << '\n';
 }
 
-void rxcpp_scan(std::vector<X> xs) {
-  auto ox = rxcpp::observable<>::create<int>([&](rxcpp::subscriber<X> s) {
-    for (auto each : xs) s.on_next(each);
+void rxcpp_scan(std::vector<I> is) {
+  auto oi = rxcpp::observable<>::create<I>([&](rxcpp::subscriber<I> s) {
+    for (auto each : is) s.on_next(each);
     s.on_completed();
   });
 
-  auto us = ox.scan(0, f).map(r);
-  us.subscribe([](U v) { std::cout << v << ' '; },
+  auto us = oi.scan(0, f).map(r);
+  us.subscribe([](O v) { std::cout << v << ' '; },
                  []() { printf("OnCompleted\n"); });
 }
 
-void range_v3(std::vector<X> xs) {
+void range_v3(std::vector<I> is) {
   using namespace ranges;
-  const auto us = xs | views::exclusive_scan(0, f) | views::transform(r);
+  const auto us = is | views::exclusive_scan(0, f) | views::transform(r);
   std::cout << us << '\n';
 }
 
 int main() {
-  const std::vector<X> xs = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  const std::vector<I> is = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-  by_hand(xs);
-  by_hand_as_list(xs);
-  rxcpp_scan(xs);
-  lib_cpp(xs);
-  range_v3(xs);
+  by_hand(is);
+  by_hand_as_list(is);
+  rxcpp_scan(is);
+  lib_cpp(is);
+  range_v3(is);
 }
