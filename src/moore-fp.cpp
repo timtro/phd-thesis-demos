11aa66cd925ccaef59bbde4ad1ca297c589a98c8
timtro <timtro@gmail.com>
2022-12-08 11:54:27 -0500

    Formatting

--- src/moore-fp.cpp
+++ src/moore-fp.cpp
@@ -10,8 +10,8 @@
 #include <rxcpp/rx.hpp>
 
 template <class T>
-std::ostream &operator<<(std::ostream &os,
-                         const std::vector<T> &v) {
+auto operator<<(std::ostream &os, const std::vector<T> &v)
+    -> std::ostream & {
   std::string separator = "[ ";
   for (const auto x : v) {
     std::cout << separator << x;
@@ -39,12 +39,10 @@ template <typename S>
 using M = std::pair<std::function<S(Input)>, Output>;
 
 // MCoalg = S → M<S> = S → ( I ⊸ S, O);
-template <typename S>
-using MCoalg = std::function<M<S>(S)>;
+template <typename S> using MCoalg = std::function<M<S>(S)>;
 
 // Λ ≅ M<Λ> = (I ⊸ Λ, O) = (I ⊸ (I ⊸ (I ⊸ (⋯), O), O), O), O)
-template <typename S>
-struct Lambda : M<Lambda<S>> {
+template <typename S> struct Lambda : M<Lambda<S>> {
   Lambda(MCoalg<S> sigma, S s0) {
     const M<S> ms = sigma(s0);
     // .first and .second come from std::pair parentage:
@@ -55,18 +53,15 @@ struct Lambda : M<Lambda<S>> {
   }
 };
 
-template <typename S>
-struct std::tuple_size<Lambda<S>> {
+template <typename S> struct std::tuple_size<Lambda<S>> {
   static constexpr std::size_t value = 2;
 };
 
-template <typename S>
-struct std::tuple_element<0, Lambda<S>> {
+template <typename S> struct std::tuple_element<0, Lambda<S>> {
   using type = std::function<Lambda<S>(Input)>;
 };
 
-template <typename S>
-struct std::tuple_element<1, Lambda<S>> {
+template <typename S> struct std::tuple_element<1, Lambda<S>> {
   using type = Output;
 };
 
@@ -86,16 +81,16 @@ auto get(const Lambda<S> &l) {
 template <typename A, typename B>
 auto mmap(std::function<B(A)> f) -> std::function<M<B>(M<A>)> {
   return [f](const M<A> ma) -> M<B> {
-    return {[f, ma](auto x) { return f(ma.first(x)); },
-            ma.second};
+    return {
+        [f, ma](auto x) { return f(ma.first(x)); }, ma.second};
   };
 }
 //                                                                          }}}1
 
 // moore_machine_...(…) {{{1
-auto moore_machine_explicit(
-    std::vector<Input> is, MooreMachine<Input, State, Output> mm)
-    -> std::vector<Output> {
+// clang-format off
+auto moore_machine_explicit(std::vector<Input> is,
+  MooreMachine<Input, State, Output> mm) -> std::vector<Output> {
   const auto [s0, f, r] = mm;
   return {
       r(s0),
@@ -105,9 +100,10 @@ auto moore_machine_explicit(
       r(f(f(f(f(s0, is[0]), is[1]), is[2]), is[3])),
       r(f(f(f(f(f(s0, is[0]), is[1]), is[2]), is[3]), is[4]))};
 }
+// clang-format on
 
 auto moore_machine_loop(const std::vector<Input> &is,
-                        MooreMachine<Input, State, Output> mm)
+    MooreMachine<Input, State, Output> mm)
     -> std::vector<Output> {
   std::vector<int> output(is.size());
   const auto [s0, f, r] = mm;
@@ -124,7 +120,7 @@ auto moore_machine_loop(const std::vector<Input> &is,
 //                                                                          }}}1
 // Using Moore fixpoint {{{1
 auto moore_lambda_explicit(const std::vector<Input> &is,
-                           MooreMachine<Input, State, Output> mm)
+    MooreMachine<Input, State, Output> mm)
     -> std::vector<Output> {
   auto s0 = mm.s0;
   auto f = mm.tmap;
@@ -147,13 +143,13 @@ auto moore_lambda_explicit(const std::vector<Input> &is,
 }
 
 // unfold : ( (A → optional<pair<A, B>>), A ) → vector<B>
-template <typename F, typename A>
-auto unfold(F f, A a0) {
+template <typename F, typename A> auto unfold(F f, A a0) {
   // will fail if `f` doesn't return a pair when given an A.
-  using B = decltype(std::declval<std::invoke_result_t<F, A>>()
-                         ->second);
+  using B =
+      decltype(std::declval<std::invoke_result_t<F, A>>()
+                   ->second);
   static_assert(std::is_same_v<std::invoke_result_t<F, A>,
-                               std::optional<std::pair<A, B>>>);
+      std::optional<std::pair<A, B>>>);
 
   std::vector<B> bs;
 
@@ -187,7 +183,7 @@ auto maybe_head_and_tail(std::vector<T> ts)
 }
 
 auto moore_lambda_listana(const std::vector<Input> &is,
-                          MooreMachine<Input, State, Output> mm)
+    MooreMachine<Input, State, Output> mm)
     -> std::vector<Output> {
 
   using LxI = std::pair<Lambda<State>, std::vector<Input>>;
@@ -200,11 +196,11 @@ auto moore_lambda_listana(const std::vector<Input> &is,
       return std::nullopt;
 
     auto [head, tail] = *opt_head_tail;
-    return { {{l.first(head), tail}, l.second} };
+    return {{{l.first(head), tail}, l.second}};
   };
 
-  MCoalg<State> sigma = [f = mm.tmap,
-                         r = mm.rmap](State s) -> M<State> {
+  MCoalg<State> sigma =
+      [f = mm.tmap, r = mm.rmap](State s) -> M<State> {
     return {[s, f, r](Input i) { return f(s, i); }, r(s)};
   };
 
@@ -213,7 +209,7 @@ auto moore_lambda_listana(const std::vector<Input> &is,
 //                                                                          }}}1
 // stdlib_cpp {{{1
 auto stdlib_cpp(const std::vector<Input> &is,
-                MooreMachine<Input, State, Output> mm)
+    MooreMachine<Input, State, Output> mm)
     -> std::vector<Output> {
   const auto [s0, f, r] = mm;
   std::vector<int> output(is.size());
@@ -225,15 +221,15 @@ auto stdlib_cpp(const std::vector<Input> &is,
 //                                                                          }}}1
 // rxcpp_scan {{{1
 auto rxcpp_scan(const std::vector<Input> &is,
-                MooreMachine<Input, State, Output> mm)
+    MooreMachine<Input, State, Output> mm)
     -> std::vector<Output> {
   const auto [s0, f, r] = mm;
-  auto oi = rxcpp::observable<>::create<Input>(
-      [&](rxcpp::subscriber<Input> s) {
-        for (auto each : is)
-          s.on_next(each);
-        s.on_completed();
-      });
+  auto oi = rxcpp::observable<>::create<
+      Input>([&](rxcpp::subscriber<Input> s) {
+    for (auto each : is)
+      s.on_next(each);
+    s.on_completed();
+  });
 
   std::vector<Output> output;
   auto us = oi.scan(0, f).map(r);
@@ -244,7 +240,7 @@ auto rxcpp_scan(const std::vector<Input> &is,
 //                                                                          }}}1
 // range_exclusive_scan {{{1
 auto range_exclusive_scan(const std::vector<Input> &is,
-                          MooreMachine<Input, State, Output> mm)
+    MooreMachine<Input, State, Output> mm)
     -> std::vector<Output> {
   const auto [s0, f, r] = mm;
 
