1181ea4a0f2c6c62985f5b57d200c6ac807a2b63
timtro <timtro@gmail.com>
2023-02-19 01:53:15 -0500

    Majore rewriting to fit Thesis narrative.

--- src/moore-fp.cpp
+++ src/moore-fp.cpp
@@ -17,6 +17,40 @@ using State = int;
 using Output = int;
 using Input = int;
 
+// Basic cCpp defs f[[[1
+template <typename T>
+constexpr decltype(auto) id(T &&x) {
+  return std::forward<T>(x);
+}
+
+template <typename F, typename... Fs>
+constexpr decltype(auto) compose(F f, Fs... fs) {
+  if constexpr (sizeof...(fs) < 1)
+    return [f](auto &&x) -> decltype(auto) {
+      return std::invoke(f, std::forward<decltype(x)>(x));
+    };
+  else
+    return [f, fs...](auto &&x) -> decltype(auto) {
+      return std::invoke(
+          f, compose(fs...)(std::forward<decltype(x)>(x)));
+    };
+}
+
+template <typename F>
+constexpr decltype(auto) curry(F f) {
+  if constexpr (std::is_invocable_v<F>)
+    return std::invoke(f);
+  else
+    return [f](auto &&x) {
+      return curry(
+          // perfectly capture x here:
+          [f, x](auto &&...xs)
+              -> decltype(std::invoke(f, x, xs...)) {
+            return std::invoke(f, x, xs...);
+          });
+    };
+}
+//                                                                         f]]]1
 // Classical Moore Machine f[[[1
 template <typename I, typename S, typename O>
 struct MooreMachine {
@@ -48,66 +82,31 @@ auto mmap(std::function<B(A)> f) -> std::function<M<B>(M<A>)> {
 template <typename S>
 using MCoalgebra = std::function<M<S>(S)>;
 
-// Λ ≅ M<Λ> = (I ⊸ Λ, O) = (I ⊸ (I ⊸ (I ⊸ (⋯), O), O), O), O)
-template <typename S>
-struct Lambda : M<Lambda<S>> {
-  Lambda(MCoalgebra<S> sigma, S s0) {
-    const M<S> ms = sigma(s0);
-    // .first and .second come from std::pair parentage:
-    this->first = [=](Input i) {
-      return Lambda(sigma, ms.first(i));
-    };
-    this->second = ms.second;
-  }
-};
-
-template <typename S>
-struct std::tuple_size<Lambda<S>> {
-  static constexpr std::size_t value = 2;
-};
-
-template <typename S>
-struct std::tuple_element<0, Lambda<S>> {
-  using type = std::function<Lambda<S>(Input)>;
-};
-
-template <typename S>
-struct std::tuple_element<1, Lambda<S>> {
-  using type = Output;
-};
-
-template <std::size_t I, typename S>
-auto get(const Lambda<S> &l) {
-  if constexpr (I == 0)
-    return l.first;
-  else
-    return l.second;
+template <typename I, typename S, typename O>
+auto moore_to_coalgebra(MooreMachine<I, S, O> mm)
+    -> MCoalgebra<S> {
+  return [mm](S s) {
+    return M<S>{curry(mm.tmap)(s), mm.rmap(s)};
+  };
 }
+
 //                                                                         f]]]1
-// Scanify solution stuff f[[[1
+// Moore algebra f[[[1
 template <typename S, typename I>
 using OP = std::optional<std::pair<S, I>>;
 
 template <typename S, typename I>
 using OPAlgebra = std::function<S(OP<S, I>)>;
 
-// scanify :: 𝘗ᵣAlgebra state value → 𝘗ᵣAlgebra (Snoc state)
-// value scanify alg (Pᵣ Nothing) = nil ⧺ alg (Pᵣ Nothing)
-// scanify alg (Pᵣ (Just ( accum , val))) = accum ⧺ alg (Pᵣ (Just
-// (s0, val)))
-//   where
-//     s0 = snocHead accum
-template <typename S, typename I>
-auto scanify(OPAlgebra<S, I> alg)
-    -> OPAlgebra<std::vector<S>, I> {
-  return [alg](OP<std::vector<S>, I> op) -> std::vector<S> {
-    if (!op)
-      return std::vector{alg(std::nullopt)};
+template <typename I, typename S, typename O>
+auto moore_to_algebra(MooreMachine<I, S, O> mm)
+    -> OPAlgebra<S, I> {
+  return [mm](OP<S, I> o_sxi) {
+    if (!o_sxi)
+      return mm.s0;
 
-    auto [accum, val] = *op;
-    auto s0 = accum.back();
-    accum.push_back(alg(OP<S, I>{{s0, val}}));
-    return accum;
+    auto [s, i] = *o_sxi;
+    return mm.tmap(s, i);
   };
 }
 
@@ -135,6 +134,24 @@ auto cata(OPAlgebra<S, I> alg, std::vector<I> vs) -> S {
     accumulator = alg({{accumulator, v}});
 
   return accumulator;
+}
+
+template <typename S, typename I>
+auto make_cata(OPAlgebra<S, I> alg)
+    -> std::function<S(std::vector<I>)> {
+
+  return [alg](std::vector<I> i_s) -> S {
+    auto s0 = alg(std::nullopt);
+
+    if (i_s.empty())
+      return s0;
+
+    auto accumulator = s0;
+    for (auto &i : i_s)
+      accumulator = alg({{accumulator, i}});
+
+    return accumulator;
+  };
 }
 // f]]]1
 // List co/algebra stuff f[[[1
@@ -175,6 +192,26 @@ auto pr_ana(OPCoalgebra<T, U> coalg, T seed) -> std::vector<U> {
   }
 }
 //                                                                         f]]]1
+// SCANIFY f[[[1 scanify :: 𝘗ᵣAlgebra state value → 𝘗ᵣAlgebra
+// (Snoc state) value scanify alg (Pᵣ Nothing) = nil ⧺ alg (Pᵣ
+// Nothing) scanify alg (Pᵣ (Just ( accum , val))) = accum ⧺ alg
+// (Pᵣ (Just (s0, val)))
+//   where
+//     s0 = snocHead accum
+template <typename S, typename I>
+auto scanify(OPAlgebra<S, I> alg)
+    -> OPAlgebra<std::vector<S>, I> {
+  return [alg](OP<std::vector<S>, I> op) -> std::vector<S> {
+    if (!op)
+      return std::vector{alg(std::nullopt)};
+
+    auto [accum, val] = *op;
+    auto s0 = accum.back();
+    accum.push_back(alg(OP<S, I>{{s0, val}}));
+    return accum;
+  };
+}
+//                                                                         f]]]1
 // Utilities f[[[1
 template <typename T>
 auto drop_first(std::vector<T> ts) -> std::vector<T> {
@@ -201,7 +238,7 @@ TEST_CASE(
     "`running_sum`…") {
   State s0 = 0;
   auto f = [](State s, Input i) -> State { return s + i; };
-  auto r = [](State s) -> Output { return s; };
+  auto r = id<State>;
   MooreMachine<Input, State, Output> mm = {s0, f, r};
 
   auto i_s = std::vector<Input>{0, 1, 2, 3, 4};
@@ -236,102 +273,14 @@ TEST_CASE(
     }
   }
 
-  AND_GIVEN(
-      "a function that explicitly uses a $Λ$ value "
-      "explicitly.") {
-
-    auto moore_lambda_explicit =
-        [&i_s, &mm]() -> std::vector<Output> {
-      auto s0 = mm.s0;  // Cannot capture structured
-      auto f = mm.tmap; //   bindings in C++17
-      auto r = mm.rmap; //   lambda-closures.
-
-      std::vector<int> output(i_s.size());
-
-      MCoalgebra<State> sigma = [f, r](State s) -> M<State> {
-        return {[s, f, r](Input i) { return f(s, i); }, r(s)};
-      };
-
-      auto [l0, o0] = Lambda(sigma, s0);
-      auto [l1, o1] = l0(i_s[0]);
-      auto [l2, o2] = l1(i_s[1]);
-      auto [l3, o3] = l2(i_s[2]);
-      auto [l4, o4] = l3(i_s[3]);
-      auto [l5, o5] = l4(i_s[4]);
-
-      return {o0, o1, o2, o3, o4, o5};
-    };
-
-    THEN("we should expect the running sum including the output "
-         "of the initial state.") {
-
-      REQUIRE(moore_lambda_explicit() == running_sum);
-    }
-  }
-
-  AND_GIVEN(
-      "a function which combines the Lambda structure and "
-      "a $𝘗$-anamorphism to automatically map the input "
-      "to the output, simulating inclusive scan.") {
-
-    auto moore_lambda_list_ana =
-        [&i_s, &mm]() -> std::vector<Output> {
-      using LxI = std::pair<Lambda<State>, std::vector<Input>>;
-
-      OPCoalgebra<LxI, Output> rho_exclusive =
-          [](LxI lambda_and_inputs) -> OP_O<LxI> {
-        auto [l, is] = lambda_and_inputs;
-        auto opt_head_tail = maybe_head_and_tail(is);
-
-        if (!opt_head_tail)
-          return std::nullopt;
-
-        auto [head, tail] = *opt_head_tail;
-        return {{{l.first(head), tail}, l.second}};
-      };
-
-      OPCoalgebra<LxI, Output> rho_inclusive =
-          [](LxI lambda_and_inputs) -> OP_O<LxI> {
-        auto [l, is] = lambda_and_inputs;
-        auto opt_head_tail = maybe_head_and_tail(is);
-
-        if (!opt_head_tail)
-          return std::nullopt;
-
-        auto [head, tail] = *opt_head_tail;
-        auto next_lambda = l.first(head);
-
-        return {{{next_lambda, tail}, next_lambda.second}};
-      };
-
-      MCoalgebra<State> sigma =
-          [f = mm.tmap, r = mm.rmap](State s) -> M<State> {
-        return {[s, f, r](Input i) { return f(s, i); }, r(s)};
-      };
-
-      return pr_ana(
-          rho_inclusive, std::pair{Lambda(sigma, mm.s0), i_s});
-    };
-
-    THEN("the function should return a running sum including "
-         "the final state but without the initial.") {
-      REQUIRE(moore_lambda_list_ana() == drop_first(running_sum));
-    }
-  }
-
-  AND_GIVEN("A 𝘗-algebra embodying f and s0") {
-    OPAlgebra<State, Input> alg =
-        [&mm](OP<State, Input> p) -> State {
-      if (!p)
-        return mm.s0;
-
-      auto [s, i] = *p;
-      return mm.tmap(s, i);
-    };
+  AND_GIVEN("A 𝘗*-algebra embodying $f$ and $s0$") {
+    OPAlgebra<State, Input> alg = moore_to_algebra(mm);
 
     THEN("The algebra catamorphised over the input list should "
-         "produce the sum") {
-      REQUIRE(cata(alg, i_s) == running_sum.back());
+         "produce the total sum") {
+
+      auto phi = compose(mm.rmap, make_cata(alg));
+      REQUIRE(phi(i_s) == running_sum.back());
     }
 
     THEN("The scanified version of that algebra should produce "
@@ -401,7 +350,8 @@ TEST_CASE(
 
       using namespace ranges;
       const auto us =
-          i_s | views::exclusive_scan(0, f) | views::transform(r);
+          i_s | views::exclusive_scan(0, f) |
+          views::transform(r);
 
       return std::vector(std::cbegin(us), std::cend(us));
     };
