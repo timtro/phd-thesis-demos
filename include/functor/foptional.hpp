f3a38d4b57faa1b0ac6fb7cad4add42ad8c391fc
timtro <timtro@gmail.com>
2023-03-01 12:42:25 -0500

    Formatted and fixed else-after-return.

--- include/functor/foptional.hpp
+++ include/functor/foptional.hpp
@@ -2,30 +2,30 @@
 
 #include "functor.hpp"
 #include <algorithm>
+#include <functional>
 #include <optional>
 #include <type_traits>
-#include <functional>
-
 
 // fmap : (A → B) → F<A> → F<B>
 //
-// Accepts optional value by reference, and then dereferences on pass to invoke.
+// Accepts optional value by reference, and then dereferences on
+// pass to invoke.
 template <typename A, typename F>
 auto fmap(F f, const std::optional<A> &oa) {
-  if (oa) {
+  if (oa)
     return std::make_optional(std::invoke(f, *oa));
-  } else
-    return std::optional<std::invoke_result_t<F, A>>{};
+
+  return std::optional<std::invoke_result_t<F, A>>{};
 }
 
 // fmap : (A → B) → F<A> → F<B>
 //
-// Takes optional by universal reference and moves from the dereferenced
-// optional.
+// Takes optional by universal reference and moves from the
+// dereferenced optional.
 template <typename A, typename F>
 auto fmap(F f, std::optional<A> &&oa) {
-  if (oa) {
+  if (oa)
     return std::make_optional(std::invoke(f, std::move(*oa)));
-  } else
-    return std::optional<std::invoke_result_t<F, A>>{};
+
+  return std::optional<std::invoke_result_t<F, A>>{};
 }
