f18df1628b4bb2bd8cd67945a3ea753c5d91f7d1
timtro <timtro@gmail.com>
2023-03-01 12:43:51 -0500

    Renaming and formatting for thesis.

--- include/functor/curried-fmap.hpp
+++ include/functor/curried-fmap.hpp
@@ -3,10 +3,12 @@
 #include <utility>
 
 namespace tf {
-  // A delayed-binding wrapper producing a curried fmap variant. That is to say,
-  // this curried version of fmap produces a function lifted into a functor, but
-  // the choice of which functor is delayed until the lifted function object is
-  // passed a value from a specific functor and the fmap overload is selected.
+  // A delayed-binding wrapper producing a curried fmap variant.
+  // That is to say, this curried version of fmap produces a
+  // function lifted into a functor, but the choice of which
+  // functor is delayed until the lifted function object is
+  // passed a value from a specific functor and the fmap overload
+  // is selected.
   template <typename F>
   auto fmap(F f) {
     return [f](auto &&xs) -> decltype(auto) {
@@ -14,16 +16,17 @@ namespace tf {
     };
   }
 
-  // A static interface for any functor. The idea here is to provide an fmap
-  // member for standard containers, like std::vector and std::options. Use:
+  // A static interface for any functor. The idea here is to
+  // provide an fmap member for standard containers, like
+  // std::vector and std::options. Use:
   //  std::vector<int> a{…};
   //  as_functor{a}.fmap(…);
   // but maybe this is a bad idea.
   template <typename Functor>
-  struct Functor_t {
+  struct FunctorT {
     const Functor &as;
 
-    Functor_t(const Functor &as) : as{as} {}
+    FunctorT(const Functor &as) : as{as} {}
 
     template <typename F>
     decltype(auto) fmap(F &&f) const {
@@ -33,6 +36,6 @@ namespace tf {
 
   template <typename Functor>
   auto as_functor(const Functor &as) {
-    return Functor_t<Functor>{as};
+    return FunctorT<Functor>{as};
   }
 } // namespace tf
