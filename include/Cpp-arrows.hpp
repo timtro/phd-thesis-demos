d2f4aa411a16091612d4747dab0a35b14f50c56b
timtro <timtro@gmail.com>
2023-03-29 10:53:56 -0400

    Fixed foldmarkers, added function_sig_helper deduction case.

--- include/Cpp-arrows.hpp
+++ include/Cpp-arrows.hpp
@@ -6,13 +6,12 @@
 #include <functional>
 #include <type_traits>
 #include <utility>
-
 #include <variant>
 
 using std::experimental::is_detected_v;
 
 namespace tf {
-// Tools for categorical notation of arrows ............... f[[[1
+  // Tools for categorical notation of arrows ............. f[[[1
   template <typename... Ts>
   struct Doms {};
 
@@ -37,6 +36,10 @@ namespace tf {
     Arg1 function_signature_helper(
         Ret (F::*)(Arg1, Rest...) const);
 
+    template <typename F, typename Ret, typename Arg1,
+        typename... Rest>
+    Arg1 function_signature_helper(Ret (*)(Arg1, Rest...));
+
     template <typename F>
     struct first_parameter {
       using type =
@@ -49,14 +52,14 @@ namespace tf {
 
   template <typename F>
   using Cod = typename std::invoke_result_t<F, Dom<F>>;
-// ........................................................ f]]]1
-// Identity function ...................................... f[[[1
+  // ...................................................... f]]]1
+  // Identity function .................................... f[[[1
   template <typename T>
   constexpr decltype(auto) id(T &&x) {
     return std::forward<T>(x);
   }
-// ........................................................ f]]]1
-// Arrow composition ...................................... f[[[1
+  // ...................................................... f]]]1
+  // Arrow composition .................................... f[[[1
   template <typename F, typename... Fs>
   constexpr decltype(auto) compose(F f, Fs... fs) {
     if constexpr (sizeof...(fs) < 1)
@@ -69,8 +72,8 @@ namespace tf {
             f, compose(fs...)(std::forward<decltype(x)>(x)));
       };
   }
-// ........................................................ f]]]1
-// Currying ............................................... f[[[1
+  // ...................................................... f]]]1
+  // Currying ............................................. f[[[1
   template <typename F>
   constexpr decltype(auto) curry(F f) {
     if constexpr (std::is_invocable_v<F>)
@@ -85,69 +88,5 @@ namespace tf {
             });
       };
   }
-// ........................................................ f]]]1
-// Fan-out/in ............................................. f[[[1
-template<typename F, typename G>
-auto fanout(F f, G g) -> Hom<Dom<F>, std::pair<Cod<F>, Cod<G>>> {
-  using T = Dom<F>;
-  using U = Cod<F>;
-  using V = Cod<G>;
-
-  static_assert(std::is_same_v<T, Dom<G>>);
-
-  return [f, g](T t) -> std::pair<U, V> {
-    return {f(t), g(t)};
-  };
-}
-
-template<typename F, typename G>
-auto prod(F f, G g)
-    -> Hom<std::pair<Dom<F>, Dom<G>>, std::pair<Cod<F>, Cod<G>>> {
-  using TandU = std::pair<Dom<F>, Dom<G>>;
-  using XandY = std::pair<Cod<F>, Cod<G>>;
-
-  return [f, g](TandU tu) -> XandY {
-    auto [t, u] = tu;
-    return {f(t), g(u)};
-  };
-}
-
-template<typename F, typename G>
-auto fanin(F f, G g) -> Hom<std::variant<Dom<F>, Dom<G>>, Cod<F>> {
-  using T = Dom<F>;
-  using U = Dom<G>;
-  using X = Cod<F>;
-  using Y = Cod<G>;
-  using TorU = std::variant<T, U>;
-
-  static_assert(std::is_same_v<X, Y>);
-
-  return [f, g](TorU t_or_u) -> X {
-    if (std::holds_alternative<T>(t_or_u))
-      return std::invoke(f, std::get<T>(t_or_u));
-    else
-      return std::invoke(g, std::get<U>(t_or_u));
-  };
-}
-
-// ((A → B), (C → D)) → (A + C → B + D)
-template<typename F, typename G>
-auto coprod(F f, G g)
-    -> Hom<std::variant<Dom<F>, Dom<G>>, std::variant<Cod<F>, Cod<G>>> {
-  using T = Dom<F>;
-  using U = Dom<G>;
-  using X = Cod<F>;
-  using Y = Cod<G>;
-  using TorU = std::variant<T, U>;
-  using XorY = std::variant<X, Y>;
-
-  return [f, g](TorU t_or_u) -> XorY {
-    if (std::holds_alternative<T>(t_or_u))
-      return std::invoke(f, std::get<T>(t_or_u));
-    else
-      return std::invoke(g, std::get<U>(t_or_u));
-  };
-}
-
-// ........................................................ f]]]1
+  // ...................................................... f]]]1
 } // namespace tf
