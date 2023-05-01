a1b73d1b89ccfee3305872f2271ff2a63c67057e
timtro <timtro@gmail.com>
2023-05-01 19:33:57 -0400

    Removed unused.

--- src/moore-fp.cpp
+++ src/moore-fp.cpp
@@ -26,155 +26,17 @@ namespace rx {
 using moore::moore_to_coalgebra;
 using moore::MooreMachine;
 using moore::snoc_scanify;
+using moore::M;
+using moore::M_map;
+using moore::MCoalgebra;
+using moore::moore_to_coalgebra;
 
 using State = moore::State;   // int
 using Output = moore::Output; // int
 using Input = moore::Input;   // int
 
-// Moore Coalgebra ........................................ f[[[1
-
-// M<S> = $(I ⊸ S, O)$
-template <typename S>
-using M = std::pair<Hom<Input, S>, Output>;
-
-//              M<𝑓>
-//         M<A> ────🢒 M<B>
-//
-//          A ───────🢒 B
-//               𝑓
-template <typename A, typename B>
-auto M_map(Hom<A, B> f) -> Hom<M<A>, M<B>> {
-  return [f](const M<A> ma) -> M<B> {
-    return {[f, ma](auto x) { return f(ma.first(x)); }, ma.second};
-  };
-}
-
-// $\mathtt{MCoalg⟨S⟩} ≅ S → 𝘔⟨S⟩ = S → ( I ⊸ S, O)$
-template <typename S>
-using MCoalgebra = Hom<S, M<S>>;
-
-template <typename I, typename S, typename O>
-auto moore_to_coalgebra(MooreMachine<I, S, O> mm) -> MCoalgebra<S> {
-  return [mm](S s) { return M<S>{curry(mm.tmap)(s), mm.rmap(s)}; };
-}
-
-// ........................................................ f]]]1
-// Moore algebra .......................................... f[[[1
-
-// $𝘖𝘗⟨S,I⟩ ≅ 𝟣 + S × I$
-template <typename S, typename I>
-using OP = std::optional<std::pair<S, I>>;
-
-// $\mathtt{OPAlgebra} = \texttt{OP}⟨S, I⟩$
-template <typename S, typename I>
-using OPAlgebra = Hom<OP<S, I>, S>;
-
-// $𝘖𝘗I⟨S⟩ ≅ 𝟣 + S × I$
-template <typename S>
-using OPI = OP<S, Input>;
-
-// $\mathtt{OPIAlgebra} = \texttt{OPI}⟨S⟩$
-template <typename S>
-using OPIAlgebra = Hom<OPI<S>, S>;
-
-template <typename S>
-auto moore_to_algebra(MooreMachine<Input, S, Output> mm)
-    -> OPIAlgebra<S> {
-  return [mm](OPI<S> o_sxi) -> S {
-    if (!o_sxi)
-      return mm.s0;
-
-    auto [s, i] = o_sxi.value();
-    return mm.tmap(s, i);
-  };
-}
-
-template <typename S>
-auto make_cata(OPIAlgebra<S> alg) -> Hom<std::vector<Input>, S> {
-
-  return [alg](std::vector<Input> iv) -> S {
-    auto s0 = alg(std::nullopt);
-
-    auto accumulator = s0;
-    for (auto &i : iv)
-      accumulator = alg({{accumulator, i}});
-
-    return accumulator;
-  };
-}
-// ........................................................ f]]]1
-// List coalgebra stuff ................................... f[[[1
-template <typename T, typename U>
-using OPCoalgebra = Hom<T, OP<T, U>>;
-
-template <typename T>
-auto maybe_head_and_tail(std::vector<T> ts)
-    -> std::optional<std::pair<T, std::vector<T>>> {
-  if (ts.empty())
-    return std::nullopt;
-
-  const T head = ts[0];
-  ts.erase(std::begin(ts));
-  return {{head, ts}};
-}
-
-template <typename T, typename U>
-auto ana_op(OPCoalgebra<T, U> coalg, T seed) -> std::vector<U> {
-  std::vector<U> us;
-
-  auto result_ab = coalg(seed);
-
-  if (result_ab)
-    us.push_back(result_ab->second);
-  else
-    return us;
-
-  while (true) {
-    result_ab = coalg(std::move(result_ab->first));
-    if (result_ab)
-      us.push_back(result_ab->second);
-    else
-      return us;
-  }
-}
-// ........................................................ f]]]1
-// Scanify ................................................ f[[[1
-//
-// scanify :: OPAlgebra<S,I> → OPAlgebra<S,I>
-// transforms an algebra so that its catamorphism produces a
-// scan.
-
-// This commented version uses OPAlgebra, and templates on input
-// type.
-//
-// template <typename S, typename I>
-// auto scanify(OPAlgebra<S, I> alg)
-//     -> OPAlgebra<std::vector<S>, I> {
-//   return [alg](OP<std::vector<S>, I> op) -> std::vector<S> {
-//     if (!op)
-//       return std::vector{alg(std::nullopt)};
-//
-//     auto [accum, val] = *op;
-//     auto s0 = accum.back();
-//     accum.push_back(alg(OP<S, I>{{s0, val}}));
-//     return accum;
-//   };
-// }
-
-template <typename S>
-auto scanify(OPIAlgebra<S> alg) -> OPIAlgebra<std::vector<S>> {
-  return [alg](OPI<std::vector<S>> op) -> std::vector<S> {
-    if (!op)
-      return std::vector{alg(std::nullopt)};
-
-    auto [accum, val] = *op;
-    auto s0 = accum.back();
-    accum.push_back(alg(OPI<S>{{s0, val}}));
-    return accum;
-  };
-}
-// ........................................................ f]]]1
 // RxCpp operators for Moore machines ..................... f[[[1
+
 template <typename I, typename S>
 auto rx_scanl(S s0, Hom<Doms<S, I>, S> f)
     -> Hom<rx::observable<I>, rx::observable<S>> {
@@ -190,8 +52,10 @@ auto rx_moore_machine(MooreMachine<I, S, O> mm)
     return i | rx_scanl(mm.s0, mm.tmap) | rx::map(mm.rmap);
   };
 }
+
 // ........................................................ f]]]1
 // Utilities .............................................. f[[[1
+
 template <typename T>
 auto drop_first(std::vector<T> ts) -> std::vector<T> {
   assert(!ts.empty());
@@ -266,30 +130,6 @@ TEST_CASE(
       "A $𝘗^*_I$-algebra embodying $f$ and $s0$, and "
       "corresponding I/O response function "
       "$\\mathtt{phi} = r ∘ ⦇\\mathtt{alg}⦈$") {
-    auto alg = moore_to_algebra(mm);
-    auto phi = compose(mm.rmap, make_cata(alg));
-
-    auto empty = std::vector<Input>();
-    auto total =
-        mm.rmap(std::accumulate(cbegin(i_s), cend(i_s), 0));
-
-    THEN("phi applied to an empty list should produce the "
-         "initial state.") {
-      REQUIRE(phi(empty) == mm.rmap(mm.s0));
-    }
-
-    THEN("phi applied to i_s should produce its sum total.") {
-      REQUIRE(phi(i_s) == total);
-    }
-
-    THEN("The scanified version of that algebra should produce "
-         "a list (i.e., std::vector) of the running sum.") {
-      auto running_sumer = make_cata(scanify(alg));
-      REQUIRE(running_sumer(i_s) == running_sum);
-    }
-  }
-
-  AND_GIVEN("Playground.") {
     auto alg = moore_to_snoc_algebra(mm);
     auto phi = compose(mm.rmap, SnocF<Input>::cata<State>(alg));
 
